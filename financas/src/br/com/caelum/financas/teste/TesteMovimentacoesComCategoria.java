69f618a77cbea2084362ef1316100fe446b94661
Nelma <nelmasb@gmail.com>
2017-04-09 15:34:50 -0300

    relacionamentos entre entidades

---
+++ financas/src/br/com/caelum/financas/teste/TesteMovimentacoesComCategoria.java
@@ -0,0 +1,56 @@
+package br.com.caelum.financas.teste;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Categoria;
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteMovimentacoesComCategoria {
+
+	public static void main(String[] args) {
+		
+		Categoria cateogira1 = new Categoria("Viagem");
+		Categoria cateogira2 = new Categoria("Negocio");
+		
+		Conta conta = new Conta();
+		conta.setId(2);
+		
+		Movimentacao movimentacao1 = new Movimentacao();
+		movimentacao1.setData(Calendar.getInstance());
+		movimentacao1.setDescricao("Viagem à SP");
+		movimentacao1.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao1.setValor(new BigDecimal("100.0"));
+		movimentacao1.setCategorias(Arrays.asList(cateogira1, cateogira2));
+		movimentacao1.setConta(conta);
+		
+		
+		Movimentacao movimentacao2 = new Movimentacao();
+		movimentacao2.setData(Calendar.getInstance());
+		movimentacao2.setDescricao("Viagem à RJ");
+		movimentacao2.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao2.setValor(new BigDecimal("300.0"));
+		movimentacao2.setCategorias(Arrays.asList(cateogira1, cateogira2));
+		movimentacao2.setConta(conta);
+
+		EntityManager manager = new JPAUtil().getEntityManager();
+		manager.getTransaction().begin();
+		
+		
+		manager.persist(cateogira1);
+		manager.persist(cateogira2);
+		
+		manager.persist(movimentacao1);
+		manager.persist(movimentacao2);
+		
+		manager.getTransaction().commit();
+		manager.close();
+	}
+
+}
